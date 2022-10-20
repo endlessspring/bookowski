e42f7e7679c1f43c0ffc0c6bbe416df4af765bc2
Mark <62847350+endlessspring@users.noreply.github.com>
2022-10-20 22:11:01 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { createI18n } from './i18n'
+
+import './App.scss'
+
+createI18n()
+
+function App() {
+
+    return (
+        <div className="App">
+
+        </div>
+    )
+}
+
+export default App
